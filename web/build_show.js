8fcdbca7942122a9ac3e93e8dcdcf02e86b103b8
Geoffrey Bachelet <grosfrais@gmail.com>
2013-08-01 19:56:18 -0400

    colorized input \o/ FUCK YEAH

--- web/build_show.js
+++ web/build_show.js
@@ -3,7 +3,7 @@
         var ws = new WebSocket('ws://stage1:8888/websocket');
 
         ws.onmessage = function(message) {
-            container.append(message.data + '\n');
+            container.append(ansi_up.ansi_to_html(message.data) + '\n');
             container[0].scrollTop = container[0].scrollHeight;
         };
     };
