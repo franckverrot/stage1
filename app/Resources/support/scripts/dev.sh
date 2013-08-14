bd1461dcd94a9149408477d6a61cc11e7d437c30
Geoffrey Bachelet <grosfrais@gmail.com>
2013-08-13 22:11:47 -0400

    install phantomjs+casperjs in the dev VM

--- app/Resources/support/scripts/dev.sh
+++ app/Resources/support/scripts/dev.sh
@@ -10,3 +10,14 @@ pip install httpie
 pip install fabric
 
 rabbitmq-plugins enable rabbitmq_management
+
+# phantomjs + casperjs
+
+apt-get install -yq \
+    libfontconfig1
+
+wget https://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-x86_64.tar.bz2 -O- | tar xzm
+wget https://github.com/n1k0/casperjs/tarball/master -O- | tar xzm
+
+(cd *phantomjs*; ln -sf $(pwd)/bin/phantomjs /usr/local/bin/phantomjs)
+(cd *casperjs*; ln -s $(pwd)/bin/casperjs /usr/local/bin/casperjs)
