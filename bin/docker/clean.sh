c48e0c884d4623b8fd28c93e991f6494ba46ea6f
Geoffrey Bachelet <grosfrais@gmail.com>
2013-08-21 21:25:59 -0400

    clean stale docker nfs handles

--- bin/docker/clean.sh
+++ bin/docker/clean.sh
@@ -7,3 +7,7 @@ XARGS="xargs --no-run-if-empty"
 docker ps -q | $XARGS docker stop
 docker ps -q -a | $XARGS docker rm
 docker images | grep -E 'none|b/' | awk '{print $3}' | $XARGS docker rmi
+
+rm -rf /var/lib/docker/containers/*
+rm -rf /var/lib/docker/grap/*
+pkill docker
