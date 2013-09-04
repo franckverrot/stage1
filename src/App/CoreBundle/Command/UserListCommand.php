3dfcb0fe0d4f66ad7ebd2ef07eefb5a7c07a8472
Geoffrey Bachelet <grosfrais@gmail.com>
2013-09-04 19:38:28 -0400

    user:list command

---
+++ src/App/CoreBundle/Command/UserListCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\CoreBundle\Command;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+
+class UserListCommand extends ContainerAwareCommand
+{
+    public function configure()
+    {
+        $this
+            ->setName('user:list')
+            ->setDescription('Retrieves a list of users');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $repository = $this->getContainer()->get('doctrine')->getRepository('AppCoreBundle:User');
+
+        foreach ($repository->findAll() as $user) {
+            $output->writeln($user->getUsername());
+        }
+   }
+}
