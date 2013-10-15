5d9b6c4e8f85fca381ab7ad4584a9695cfcbe6ff
Geoffrey Bachelet <grosfrais@gmail.com>
2013-10-15 16:27:23 +0200

    Project removal cleanup
    Project access and routing tables are now removed when a project is
    removed.

---
+++ src/App/CoreBundle/EventListener/ProjectAccessTableSubscriber.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\CoreBundle\EventListener;
+
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\Common\EventSubscriber;
+
+use Psr\Log\LoggerInterface;
+
+use App\CoreBundle\Entity\Project;
+
+use Redis;
+
+class ProjectAccessTableSubscriber implements EventSubscriber
+{
+    private $redis;
+
+    public function __construct(Redis $redis, LoggerInterface $logger)
+    {
+        $this->redis = $redis;
+        $this->logger = $logger;
+    }
+
+    public function getSubscribedEvents()
+    {
+        return ['preRemove'];
+    }
+
+    public function preRemove(LifecycleEventArgs $args)
+    {
+        $project = $args->getEntity();
+
+        if (!$project instanceof Project) {
+            return;
+        }
+
+        $this->logger->info('removing auth table for "'.$project->getSlug().'"');
+        $this->redis->del('auth:'.$project->getSlug());
+    }
+}
---
+++ src/App/CoreBundle/EventListener/ProjectRoutingTableSubscriber.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\CoreBundle\EventListener;
+
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\Common\EventSubscriber;
+
+use Psr\Log\LoggerInterface;
+
+use App\CoreBundle\Entity\Project;
+
+use Redis;
+
+/**
+ * This subscriber removes a project's hipache routing information
+ * on project's removal
+ */
+class ProjectRoutingTableSubscriber implements EventSubscriber
+{
+    private $redis;
+
+    public function __construct(Redis $redis, LoggerInterface $logger)
+    {
+        $this->redis = $redis;
+        $this->logger = $logger;
+    }
+
+    public function getSubscribedEvents()
+    {
+        return ['preRemove'];
+    }
+
+    public function preRemove(LifecycleEventArgs $args)
+    {
+        $project = $args->getEntity();
+
+        if (!$project instanceof Project) {
+            return;
+        }
+
+        $this->logger->info('removing routing informations for "'.$project->getSlug().'"');
+
+        $redis = $this->redis;
+
+        $keys = $redis->keys('frontend:*'.$project->getSlug().'*');
+        $redis->multi();
+
+        foreach ($keys as $key) {
+            $redis->del($key);
+        }
+
+        $redis->exec();
+    }
+}
