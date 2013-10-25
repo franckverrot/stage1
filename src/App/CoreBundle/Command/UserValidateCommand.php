edaf8231b464b942380e4bcecf7af20070827bcb
Geoffrey Bachelet <grosfrais@gmail.com>
2013-10-25 11:23:29 +0200

    user:validate command

---
+++ src/App/CoreBundle/Command/UserValidateCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\CoreBundle\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+
+use App\CoreBundle\Entity\User;
+
+class UserValidateCommand extends ContainerAwareCommand
+{
+    public function configure()
+    {
+        $this
+            ->setName('user:validate')
+            ->setDefinition([
+                new InputArgument('user_spec', InputArgument::REQUIRED, 'The user'),
+            ]);
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $user = $this->findUser($input->getArgument('user_spec'));
+
+        $output->writeln('validating user <info>'.$user->getUsername().'</info>');
+
+        $user->setStatus(User::STATUS_ENABLED);
+
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $em->persist($user);
+        $em->flush();
+    }
+
+    private function findUser($spec)
+    {
+        $repository = $this->getContainer()->get('doctrine')->getRepository('AppCoreBundle:User');
+
+        if (is_numeric($spec)) {
+            return $repository->find((integer) $spec);
+        }
+
+        $user = $repository->findOneByUsername($spec);
+
+        if (!$user) {
+            throw new InvalidArgumentException('User not found');
+        }
+
+        return $user;
+    }
+}
