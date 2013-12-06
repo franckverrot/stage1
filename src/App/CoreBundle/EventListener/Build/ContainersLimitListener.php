753f20dba28f06d75d86ad7729d6b3b7683b9ab7
Geoffrey Bachelet <grosfrais@gmail.com>
2013-12-06 19:59:49 +0100

    disable running containers limit for demo builds

--- src/App/CoreBundle/EventListener/Build/ContainersLimitListener.php
+++ src/App/CoreBundle/EventListener/Build/ContainersLimitListener.php
@@ -55,7 +55,7 @@ class ContainersLimitListener
     {
         $build = $event->getBuild();
 
-        if (!$build->isRunning()) {
+        if (!$build->isRunning() || $build->isDemo()) {
             return;
         }
 
