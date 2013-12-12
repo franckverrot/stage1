cf715e5d1b39506bcbc4df317747ac90ca2b8b71
Geoffrey Bachelet <grosfrais@gmail.com>
2013-12-12 18:06:07 +0100

    only show running/building/failed builds in Admin

--- src/App/AdminBundle/Controller/DefaultController.php
+++ src/App/AdminBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\AdminBundle\Controller;
 
+use App\CoreBundle\Entity\Build;
+
 class DefaultController extends Controller
 {
     public function indexAction()
@@ -18,7 +20,11 @@ class DefaultController extends Controller
 
         $builds = $doctrine
             ->getRepository('AppCoreBundle:Build')
-            ->findBy([], ['createdAt' => 'DESC'], 20);
+            ->findBy([
+                'status' => [Build::STATUS_BUILDING, Build::STATUS_RUNNING, Build::STATUS_FAILED],
+            ], [
+                'createdAt' => 'DESC'
+            ], 20);
 
         return $this->render('AppAdminBundle:Default:index.html.twig', [
             'projects' => $projects,
