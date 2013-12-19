f3c346e2084fc67270f1180c9b7978d6eafdb851
Geoffrey Bachelet <grosfrais@gmail.com>
2013-12-19 16:39:08 +0100

    docker output consumer now stores logs in redis

--- src/App/CoreBundle/Consumer/DockerOutputConsumer.php
+++ src/App/CoreBundle/Consumer/DockerOutputConsumer.php
@@ -2,82 +2,67 @@
 
 namespace App\CoreBundle\Consumer;
 
-use Symfony\Bridge\Doctrine\RegistryInterface;
-
 use App\CoreBundle\Entity\Build;
-use App\CoreBundle\Entity\BuildLog;
+
+use Docker\Docker;
 
 use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
-use OldSound\RabbitMqBundle\RabbitMq\Producer;
 use PhpAmqpLib\Message\AMQPMessage;
 
 use Psr\Log\LoggerInterface;
 
-use Docker\Docker;
+use Redis;
 
 class DockerOutputConsumer implements ConsumerInterface
 {
     private $logger;
 
-    private $doctrine;
+    private $redis;
 
-    private $docker;
+    private $streamMap = [0 => 'stdin', 1 => 'stdout', 2 => 'stderr'];
 
-    public function __construct(LoggerInterface $logger, RegistryInterface $doctrine, Docker $docker, Producer $producer)
+    public function __construct(LoggerInterface $logger, Redis $redis)
     {
         $this->logger = $logger;
-        $this->doctrine = $doctrine;
-        $this->docker = $docker;
-        $this->producer = $producer;
+        $this->redis = $redis;
 
         $this->logger->info('initialized '.__CLASS__);
+    }
+
+    private function getStreamType($type)
+    {
+        return isset($this->streamMap[$type]) ? $this->streamMap[$type] : null;
     }
 
     public function execute(AMQPMessage $message)
     {
         $logger = $this->logger;
+        $redis = $this->redis;
 
         $body = json_decode($message->body, true);
 
         if (!array_key_exists('BUILD_ID', $body['env'])) {
-            $logger->warn('no build information', ['message' => $body]);
-            return;
-        }
-
-        $em = $this->doctrine->getManager();
-        $repo = $em->getRepository('AppCoreBundle:Build');
-        $build = $repo->find($body['env']['BUILD_ID']);
-
-        if (!$build) {
-            $logger->warn('could not find build for container', [
-                'build' => $body['env']['BUILD_ID'],
-                'container' => $body['container']
+            $logger->warn('no build information', [
+                'container' => $body['container'],
+                'message' => $body
             ]);
 
             return;
         }
 
+        $buildId = $body['env']['BUILD_ID'];
+
         $logger->debug('processing log fragment', [
-            'build' => $build->getId(),
+            'build' => $buildId,
             'container' => $body['container'],
         ]);
 
-        $fragment = $body['content'];
-
-        $streamMap = [0 => 'stdin', 1 => 'stdout', 2 => 'stderr'];
-
-        $buildLog = new BuildLog();
-        $buildLog->setType(Build::LOG_OUTPUT);
-        $buildLog->setMessage($fragment);
-        $buildLog->setStream($body['type'] ? $streamMap[$body['type']] : null);
-        $buildLog->setMicrotime($body['timestamp']);
-        $buildLog->setBuild($build);
-
-        $build->addLog($buildLog);
-
-        $em->persist($build);
-        $em->persist($buildLog);
-
-        $em->flush();
+        $redis->rpush('build:output:'.$buildId, json_encode([
+            'type' => Build::LOG_OUTPUT,
+            'message' => $body['content'],
+            'stream' => $this->getStreamType($body['type']),
+            'microtime' => $body['timestamp'],
+            'build_id' => $buildId,
+        ]));
     }
 }
