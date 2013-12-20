a791cd9a81507be73635b20072eb2c635c52bc8b
Geoffrey Bachelet <grosfrais@gmail.com>
2013-12-20 15:21:49 +0100

    build:kill command

---
+++ src/App/CoreBundle/Command/BuildKillCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\CoreBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class BuildKillCommand extends ContainerAwareCommand
+{
+    public function configure()
+    {
+        $this
+            ->setName('build:kill')
+            ->setDefinition([
+                new InputArgument('build_id', InputArgument::REQUIRED, 'The build id'),
+            ]);
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $producer = $this->getContainer()->get('old_sound_rabbit_mq.kill_producer');
+        $producer->publish(json_encode(['build_id' => $input->getArgument('build_id')]));
+    }
+}
