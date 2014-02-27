ae319171ff5037f9ca5dead0e364b7884ce2567e
Geoffrey Bachelet <grosfrais@gmail.com>
2014-02-27 12:56:13 +0100

    detect when there is an error loading output logs

--- web/js/Scroller.js
+++ web/js/Scroller.js
@@ -37,35 +37,57 @@ var Scroller = function(container, url, options) {
 
         var scroller = this;
 
-        $.get(url, { page: page }, function(data) {
-            var currentTopElement = $(scroller.container).children().first();
+        $.ajax({
+            url: url,
+            data: { page: page },
+            success: function(data) {
+                var currentTopElement = $(scroller.container).children().first();
 
-            for (i = data.items.length; i > 0; i--) {
-                var fragment = data.items[i - 1];
-                $(scroller.container).prepend('<span id="fragment-' + fragment.fragment_id + '">' + ansi_up.ansi_to_html(fragment.message) + '</span>');
+                for (i = data.items.length; i > 0; i--) {
+                    var fragment = data.items[i - 1];
+                    $(scroller.container).prepend('<span id="fragment-' + fragment.fragment_id + '">' + ansi_up.ansi_to_html(fragment.message) + '</span>');
 
-                if (currentTopElement.length > 0 && !scroller.preLoading) {
-                    scroller.container.scrollTop = currentTopElement.position().top;
+                    if (currentTopElement.length > 0 && !scroller.preLoading) {
+                        scroller.container.scrollTop = currentTopElement.position().top;
+                    } else {
+                        scroller.container.scrollTop = scroller.container.scrollHeight;
+                    }
+                }
+
+                var delta = scroller.container.scrollHeight - scroller.initialScrollHeight;
+
+                if (delta <= scroller.options.threshold && data.previous_page !== null) {
+                    scroller.preLoading = true;
+                    scroller.loadPage(data.previous_page, callback);
                 } else {
-                    scroller.container.scrollTop = scroller.container.scrollHeight;
+                    scroller.preLoading = false;
+                    scroller.nextPage = data.previous_page;
+                    scroller.loading = false;
+
+                    if (typeof(callback) === 'function') {
+                        callback(data, scroller);
+                    }
+
+                    $(scroller.options.loadingIndicator).hide();                
                 }
-            }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                var container = $(scroller.container).parent();
+                $(scroller.container).remove();
 
-            var delta = scroller.container.scrollHeight - scroller.initialScrollHeight;
+                if (jqXHR.responseJSON) {
+                    if (false && jqXHR.responseJSON.total === 0) {
+                        $(container).prepend('<p class="lead">There are no output logs for this build.</p>');
+                    } else {
+                        $(container).prepend('<p class="lead">Loading output logs failed with the following message: <strong>' + jqXHR.responseJSON.error + '</strong>.</p>');
+                    }
+                } else {
+                    $(container).prepend('<p class="lead">Loading output logs failed with the following message: <strong>' + error + '</strong>.</p>');
+                }
 
-            if (delta <= scroller.options.threshold && data.previous_page !== null) {
-                scroller.preLoading = true;
-                scroller.loadPage(data.previous_page, callback);
-            } else {
+                $(scroller.options.loadingIndicator).hide();
                 scroller.preLoading = false;
-                scroller.nextPage = data.previous_page;
                 scroller.loading = false;
-
-                if (typeof(callback) === 'function') {
-                    callback(data, scroller);
-                }
-
-                $(scroller.options.loadingIndicator).hide();                
             }
         });
     }
