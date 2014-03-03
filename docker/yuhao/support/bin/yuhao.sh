88a79df080e6fbc5a47fcc21ece31fa60ec89460
Geoffrey Bachelet <grosfrais@gmail.com>
2014-03-03 11:52:32 +0100

    fixed yuhao script

--- docker/yuhao/support/bin/yuhao.sh
+++ docker/yuhao/support/bin/yuhao.sh
@@ -12,8 +12,8 @@ APP_ROOT=/app
 git clone --depth 1 --branch $2 $1 $APP_ROOT > /dev/null
 
 # check if we must (or want to) use the local build.yml configuration
-if [[ -n "$FORCE_LOCAL_BUILD_YML" || (! -f ./.build.yml && -f /root/build_local.yml) ]]; then
-    cp /root/build_local.yml ./.build.yml
+if [[ -n "$FORCE_LOCAL_BUILD_YML" || (! -f $APP_ROOT/.build.yml && -f /root/build_local.yml) ]]; then
+    cp /root/build_local.yml $APP_ROOT/.build.yml
 fi
 
 yuhao.phar $APP_ROOT
