73ed337fdc81a8d97692dc3263246129918e66d4
Geoffrey Bachelet <grosfrais@gmail.com>
2014-03-09 16:12:44 +0100

    added branchPolicy and branchPatterns project settings

--- src/App/CoreBundle/Entity/ProjectSettings.php
+++ src/App/CoreBundle/Entity/ProjectSettings.php
@@ -80,4 +80,60 @@ class ProjectSettings
     {
         return $this->project;
     }
+    /**
+     * @var string
+     */
+    private $branchPolicy;
+
+    /**
+     * @var array
+     */
+    private $branchPatterns;
+
+
+    /**
+     * Set branchPolicy
+     *
+     * @param string $branchPolicy
+     * @return ProjectSettings
+     */
+    public function setBranchPolicy($branchPolicy)
+    {
+        $this->branchPolicy = $branchPolicy;
+    
+        return $this;
+    }
+
+    /**
+     * Get branchPolicy
+     *
+     * @return string 
+     */
+    public function getBranchPolicy()
+    {
+        return $this->branchPolicy;
+    }
+
+    /**
+     * Set branchPatterns
+     *
+     * @param array $branchPatterns
+     * @return ProjectSettings
+     */
+    public function setBranchPatterns($branchPatterns)
+    {
+        $this->branchPatterns = $branchPatterns;
+    
+        return $this;
+    }
+
+    /**
+     * Get branchPatterns
+     *
+     * @return array 
+     */
+    public function getBranchPatterns()
+    {
+        return $this->branchPatterns;
+    }
 }
