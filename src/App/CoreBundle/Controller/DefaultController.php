3db66fdcf98d83444ad0edd83ee33190c45aeac2
Geoffrey Bachelet <grosfrais@gmail.com>
2014-03-12 14:40:02 +0100

    moved project actions to ProjectController

--- src/App/CoreBundle/Controller/DefaultController.php
+++ src/App/CoreBundle/Controller/DefaultController.php
@@ -28,90 +28,6 @@ class DefaultController extends Controller
         return $this->render('AppCoreBundle:Default:dashboard.html.twig');
     }
 
-    public function projectDeleteAction(Request $request, $id)
-    {
-        $project = $this->findProject($id);
-
-        $session = $this->get('session');
-        $flash = $session->getFlashBag();
-
-        if ($request->request->get('name') !== $project->getName()) {
-            $flash->add('delete-error', 'Project name validation failed.');
-            return $this->redirect($this->generateUrl('app_core_project_admin', ['id' => $id]));
-        }
-
-        if ($request->request->get('csrf_token') !== $session->get('csrf_token')) {
-            $flash->add('delete-error', 'CSRF validation failed.');
-            return $this->redirect($this->generateUrl('app_core_project_admin', ['id' => $id]));
-        }
-
-        $this->removeAndFlush($project);
-
-        $flash->add('success', sprintf('Project <strong>%s</strong> has been deleted.', $project->getName()));
-
-        return $this->redirect($this->generateUrl('app_core_homepage'));
-    }
-
-    public function projectShowAction($id)
-    {
-        return $this->redirect($this->generateUrl('app_core_project_branches', ['id' => $id]));
-    }
-
-    public function projectBuildsAction($id, $all = false)
-    {
-        $this->setCurrentProjectId($id);
-
-        $project = $this->findProject($id);
-
-        $qb = $this->getDoctrine()->getRepository('Model:Build')->createQueryBuilder('b');
-
-        $builds = $qb
-            ->leftJoin('b.branch', 'br')
-            ->where($qb->expr()->eq('b.project', ':project'))
-            ->andWhere('br.deleted = 0')
-            ->orderBy('b.createdAt', 'DESC')
-            ->setParameter(':project', $project->getId())
-            ->getQuery()
-            ->setMaxResults(100)
-            ->execute();
-
-        $running_builds = array_filter($builds, function($build) { return $build->isRunning(); });
-        $pending_builds = array_filter($builds, function($build) { return $build->isPending(); });
-        $other_builds = array_filter($builds, function($build) { return !($build->isRunning() || $build->isPending()); });
-
-        return $this->render('AppCoreBundle:Default:projectBuilds.html.twig', [
-            'project' => $project,
-            'other_builds' => $other_builds,
-            'running_builds' => $running_builds,
-            'pending_builds' => $pending_builds,
-            'has_builds' => count($builds) > 0,
-            'all' => (bool) $all,
-        ]);
-    }
-
-    public function projectScheduleBuildAction(Request $request, $id)
-    {
-        try {
-            $project = $this->findProject($id);
-            $ref = $request->request->get('ref');
-            $hash = $this->getHashFromRef($project, $ref);
-
-            $scheduler = $this->container->get('app_core.build_scheduler');
-            $build = $scheduler->schedule($project, $ref, $hash, $this->getUser());
-
-            return new JsonResponse([
-                'build_url' => $this->generateUrl('app_core_build_show', ['id' => $build->getId()]),
-                'cancel_url' => $this->generateUrl('app_core_build_cancel', ['id' => $build->getId()]),
-                'build' => $build->asMessage(),
-            ], 201);
-        } catch (Exception $e) {
-            $this->container->get('logger')->error($e->getMessage());
-            $this->container->get('logger')->error($e->getResponse()->getBody(true));
-
-            return new JsonResponse(['class' => 'danger', 'message' => $e->getMessage()], 500);
-        }
-    }
-
     # @todo move to ProjectController
     public function projectsImportAction()
     {
@@ -140,5 +56,5 @@ class DefaultController extends Controller
         ]));
 
         return new JsonResponse(json_encode(true));
-    }
+    }    
 }
--- src/App/CoreBundle/Controller/ProjectController.php
+++ src/App/CoreBundle/Controller/ProjectController.php
@@ -34,6 +34,90 @@ class ProjectController extends Controller
         ]);
     }
 
+    public function deleteAction(Request $request, $id)
+    {
+        $project = $this->findProject($id);
+
+        $session = $this->get('session');
+        $flash = $session->getFlashBag();
+
+        if ($request->request->get('name') !== $project->getName()) {
+            $flash->add('delete-error', 'Project name validation failed.');
+            return $this->redirect($this->generateUrl('app_core_project_admin', ['id' => $id]));
+        }
+
+        if ($request->request->get('csrf_token') !== $session->get('csrf_token')) {
+            $flash->add('delete-error', 'CSRF validation failed.');
+            return $this->redirect($this->generateUrl('app_core_project_admin', ['id' => $id]));
+        }
+
+        $this->removeAndFlush($project);
+
+        $flash->add('success', sprintf('Project <strong>%s</strong> has been deleted.', $project->getName()));
+
+        return $this->redirect($this->generateUrl('app_core_homepage'));
+    }
+
+    public function showAction($id)
+    {
+        return $this->redirect($this->generateUrl('app_core_project_branches', ['id' => $id]));
+    }
+
+    public function buildsAction($id, $all = false)
+    {
+        $this->setCurrentProjectId($id);
+
+        $project = $this->findProject($id);
+
+        $qb = $this->getDoctrine()->getRepository('Model:Build')->createQueryBuilder('b');
+
+        $builds = $qb
+            ->leftJoin('b.branch', 'br')
+            ->where($qb->expr()->eq('b.project', ':project'))
+            ->andWhere('br.deleted = 0')
+            ->orderBy('b.createdAt', 'DESC')
+            ->setParameter(':project', $project->getId())
+            ->getQuery()
+            ->setMaxResults(100)
+            ->execute();
+
+        $running_builds = array_filter($builds, function($build) { return $build->isRunning(); });
+        $pending_builds = array_filter($builds, function($build) { return $build->isPending(); });
+        $other_builds = array_filter($builds, function($build) { return !($build->isRunning() || $build->isPending()); });
+
+        return $this->render('AppCoreBundle:Project:builds.html.twig', [
+            'project' => $project,
+            'other_builds' => $other_builds,
+            'running_builds' => $running_builds,
+            'pending_builds' => $pending_builds,
+            'has_builds' => count($builds) > 0,
+            'all' => (bool) $all,
+        ]);
+    }
+
+    public function scheduleBuildAction(Request $request, $id)
+    {
+        try {
+            $project = $this->findProject($id);
+            $ref = $request->request->get('ref');
+            $hash = $this->getHashFromRef($project, $ref);
+
+            $scheduler = $this->container->get('app_core.build_scheduler');
+            $build = $scheduler->schedule($project, $ref, $hash, $this->getUser());
+
+            return new JsonResponse([
+                'build_url' => $this->generateUrl('app_core_build_show', ['id' => $build->getId()]),
+                'cancel_url' => $this->generateUrl('app_core_build_cancel', ['id' => $build->getId()]),
+                'build' => $build->asMessage(),
+            ], 201);
+        } catch (Exception $e) {
+            $this->container->get('logger')->error($e->getMessage());
+            $this->container->get('logger')->error($e->getResponse()->getBody(true));
+
+            return new JsonResponse(['class' => 'danger', 'message' => $e->getMessage()], 500);
+        }
+    }
+
     /**
      * @param integer $id
      */
