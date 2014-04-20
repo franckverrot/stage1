77b16aeb98f009fdf840c86a88ee9b773a715c47
Geoffrey Bachelet <grosfrais@gmail.com>
2014-04-20 15:38:13 +0200

    fixed docker clean script

--- bin/docker/soft-clean.sh
+++ bin/docker/soft-clean.sh
@@ -1,6 +1,6 @@
-#!/bin/bash -x
+#!/bin/bash -ex
 
 XARGS="xargs --no-run-if-empty"
 
-docker ps -a | grep 'Exit 0' | awk '{print $1}' | $XARGS -n 1 docker rm
-docker images -q | $XARGS docker rmi
+docker ps -a | grep 'Exit' | awk '{print $1}' | $XARGS -n 1 docker rm
+docker images | grep -E 'b/|none' | awk '{print $3}' | $XARGS docker rmi
