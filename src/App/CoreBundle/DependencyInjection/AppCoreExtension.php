1cbf7b32eabd5534b683fd56e30f9c54a10b3131
Alexandre 'pocky' Balmes <albalmes@gmail.com>
2014-04-28 14:37:45 +0200

    Move services in config/services

--- src/App/CoreBundle/DependencyInjection/AppCoreExtension.php
+++ src/App/CoreBundle/DependencyInjection/AppCoreExtension.php
@@ -19,7 +19,7 @@ class AppCoreExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config/services'));
 
         foreach (array('controllers', 'form', 'listeners', 'misc', 'model', 'rabbitmq', 'twig') as $config) {
             $loader->load(sprintf('%s.xml', $config));
