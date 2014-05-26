5270a482d6c93903059bf2cb71c20685296fd7b2
Geoffrey Bachelet <grosfrais@gmail.com>
2014-05-26 11:21:32 +0200

    pre-created mysql database in symfony2 base image

--- docker/symfony2/support/bin/stage1_image_init_build.sh
+++ docker/symfony2/support/bin/stage1_image_init_build.sh
@@ -3,3 +3,11 @@
 source /usr/local/lib/stage1.sh
 
 /etc/init.d/mysql start 2>&1 > /dev/null
+
+RET=1
+while [[ RET -ne 0 ]]; do
+    sleep 1
+    mysql -e 'exit'; RET=$?
+done
+
+mysqladmin -u root create symfony
