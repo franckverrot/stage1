1fdc098063a3ecbd4f050651afd8050ae24d9074
Geoffrey Bachelet <grosfrais@gmail.com>
2014-05-26 11:22:20 +0200

    fixed yuhao build

--- bin/yuhao/update.sh
+++ bin/yuhao/update.sh
@@ -2,11 +2,11 @@
 
 YUHAO_DIR=/projects/yuhao
 
-# if [ -d $YUHAO_DIR ]; then
-#     cd $YUHAO_DIR
-#     bin/box build
-#     cd - > /dev/null
-#     mv $YUHAO_DIR/yuhao.phar /vagrant/docker/yuhao/support/bin/yuhao.phar
-# fi;
+if [ -d $YUHAO_DIR ]; then
+    cd $YUHAO_DIR
+    bin/box build
+    cd - > /dev/null
+    mv $YUHAO_DIR/yuhao.phar /vagrant/docker/yuhao/support/bin/yuhao.phar
+fi;
 
 docker build --tag stage1/yuhao --rm --no-cache docker/yuhao
