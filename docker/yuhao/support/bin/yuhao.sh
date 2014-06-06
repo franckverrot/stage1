cd68604de39ead11d69f000309c97deb10c3b32e
Geoffrey Bachelet <grosfrais@gmail.com>
2014-06-06 22:46:37 +0200

    updated yuhao

--- docker/yuhao/support/bin/yuhao.sh
+++ docker/yuhao/support/bin/yuhao.sh
@@ -4,14 +4,16 @@ set -e
 
 APP_ROOT=/app
 
-if [ "$IS_PULL_REQUEST" = "1" ]; then
-    git clone --quiet --depth 1 $SSH_URL $APP_ROOT > /dev/null 2>&1
-    cd $APP_ROOT
-    git fetch --quiet origin refs/$REF > /dev/null  2>&1
-    git checkout --quiet -b pull_request FETCH_HEAD > /dev/null
-    cd - > /dev/null
-else
-    git clone --quiet --depth 1 --branch $REF $SSH_URL $APP_ROOT > /dev/null
+if [ ! -d $APP_ROOT ]; then
+    if [ "$IS_PULL_REQUEST" = "1" ]; then
+        git clone --quiet --depth 1 $SSH_URL $APP_ROOT > /dev/null 2>&1
+        cd $APP_ROOT
+        git fetch --quiet origin refs/$REF > /dev/null  2>&1
+        git checkout --quiet -b pull_request FETCH_HEAD > /dev/null
+        cd - > /dev/null
+    else
+        git clone --quiet --depth 1 --branch $REF $SSH_URL $APP_ROOT > /dev/null
+    fi
 fi
 
 # check if we must (or want to) use the local .stage1.yml configuration
